328a24fa5a9f1b752322099eb13ee678c71b1182
MuhammadJamshaidGhaffar <muhammadjamshaid89@gmail.com>
2022-07-09 10:54:55 +0500

    Completed

--- script.js
+++ script.js
@@ -8,9 +8,27 @@ let aliceQueenMovement = aliceQueen.animate(
   }
 );
 
+let backgroundMovement = document
+  .querySelector(".background")
+  .animate(
+    [{ transform: "translateX(118rem)" }, { transform: "translateX(-380rem)" }],
+    {
+      duration: 15000,
+      iterations: Infinity,
+    }
+  );
+
 function moveFaster() {
   aliceQueenMovement.playbackRate *= 1.1;
+  backgroundMovement.playbackRate *= 1.1;
 }
+
+setInterval(() => {
+  if (aliceQueenMovement.playbackRate > 0.4) {
+    aliceQueenMovement.playbackRate *= 0.9;
+    backgroundMovement.playbackRate *= 0.9;
+  }
+}, 2000);
 
 document.addEventListener("click", moveFaster);
 document.addEventListener("touchstart", moveFaster);
